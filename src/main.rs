7e12bd46d6d6a9389caf45899b5fa1ec23909b54
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-08-11 01:58:13 +0800

    Add fps counter.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,21 @@ mod game_state;
 mod menu;
 mod utility;
 
+use bevy_dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin};
 use game_state::GameState;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(FpsOverlayPlugin {
+            config: FpsOverlayConfig {
+                text_config: TextStyle {
+                    font_size: 40.0,
+                    color: Color::srgb(0.0, 1.0, 0.0),
+                    font: default(),
+                },
+            },
+        })
         .init_state::<GameState>()
         .add_systems(Startup, setup)
         .add_plugins((menu::plugin::setup, game::plugin::setup))
