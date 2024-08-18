ac476767e53462f1b582cf95320f1bf2adcf09a9
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-08-19 06:38:23 +0800

    F11 for toggling window/fullscreen mode, ESC for quit.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use bevy::{color::palettes::css::GREEN, prelude::*, window::WindowResolution};
+use bevy::{
+    color::palettes::css::GREEN,
+    prelude::*,
+    window::{PresentMode, WindowMode, WindowResolution},
+};
 
 mod app_state;
 mod controller;
@@ -30,7 +34,8 @@ fn main() {
         })
         .insert_resource(ClearColor(Color::BLACK)) // application background color
         .init_state::<AppState>()
-        .add_systems(Startup, setup)
+        .add_systems(Startup, setup_camera)
+        .add_systems(Update, global_handle_input_system)
         .add_plugins((
             controller::setup,
             splash::plugin::setup,
@@ -40,6 +45,30 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn global_handle_input_system(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut app_exit_events: ResMut<Events<bevy::app::AppExit>>,
+    mut window: Query<&mut Window>,
+) {
+    if keys.just_pressed(KeyCode::Escape) {
+        app_exit_events.send(bevy::app::AppExit::Success);
+    }
+    if keys.just_pressed(KeyCode::F11) {
+        let mut window = window.single_mut();
+        match window.mode {
+            WindowMode::Windowed => {
+                window.mode = WindowMode::Fullscreen;
+                window.present_mode = PresentMode::AutoNoVsync;
+            }
+            WindowMode::Fullscreen => {
+                window.mode = WindowMode::Windowed;
+                window.present_mode = PresentMode::AutoVsync;
+            }
+            _ => (),
+        }
+    }
 }
