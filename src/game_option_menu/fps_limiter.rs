f592c17b9db82749c92f03359ffeb443e6ae5a15
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-10-08 08:50:38 +0800

    Change the lowest fps to 240 to provide minimal smoothness for game playing.

--- src/game_option_menu/fps_limiter.rs
+++ src/game_option_menu/fps_limiter.rs
@@ -7,12 +7,9 @@ use num_traits::FromPrimitive;
 pub enum FPSLimiter {
     #[default]
     Unlimited,
-    Auto,
-    F60,
-    F144,
     F240,
-    F360,
     F480,
+    F960,
 }
 
 impl FPSLimiter {
@@ -48,12 +45,9 @@ impl FPSLimiter {
         let ft = |fps| Duration::from_secs_f32(1.0 / fps as f32);
         match self {
             FPSLimiter::Unlimited => bevy_framepace::Limiter::Off,
-            FPSLimiter::Auto => bevy_framepace::Limiter::Auto,
-            FPSLimiter::F60 => bevy_framepace::Limiter::Manual(ft(60)),
-            FPSLimiter::F144 => bevy_framepace::Limiter::Manual(ft(144)),
             FPSLimiter::F240 => bevy_framepace::Limiter::Manual(ft(240)),
-            FPSLimiter::F360 => bevy_framepace::Limiter::Manual(ft(360)),
             FPSLimiter::F480 => bevy_framepace::Limiter::Manual(ft(480)),
+            FPSLimiter::F960 => bevy_framepace::Limiter::Manual(ft(960)),
         }
     }
 }
@@ -61,13 +55,10 @@ impl FPSLimiter {
 impl Display for FPSLimiter {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            FPSLimiter::Auto => f.write_str("AUTO"),
             FPSLimiter::Unlimited => f.write_str("UNLIMITED"),
-            FPSLimiter::F60 => f.write_str("60 FPS"),
-            FPSLimiter::F144 => f.write_str("144 FPS"),
             FPSLimiter::F240 => f.write_str("240 FPS"),
-            FPSLimiter::F360 => f.write_str("360 FPS"),
             FPSLimiter::F480 => f.write_str("480 FPS"),
+            FPSLimiter::F960 => f.write_str("960 FPS"),
         }
     }
 }
