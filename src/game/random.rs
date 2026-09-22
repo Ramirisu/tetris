d6d72d25e9644d91749197f2140800775a1f9552
Ramirisu <labyrinth.ramirisu@gmail.com>
2026-09-22 20:53:22 +0800

    Refactor piece generation methods in `Random` enum for clarity and consistency

--- src/game/random.rs
+++ src/game/random.rs
@@ -24,36 +24,37 @@ impl Random {
     pub fn gen_pieces<R: rand::Rng>(&self, rng: &mut R, history: &PieceHistory) -> Vec<Piece> {
         match self {
             Random::Uniform => vec![Self::gen_piece_uniform(rng)],
-            Random::Classic => Self::gen_pieces_1h2r(rng, history),
-            Random::Modern => Self::gen_pieces_7bag(rng, history),
+            Random::Classic => vec![Self::gen_piece_1h2r(rng, history)],
+            Random::Modern => Self::gen_bag(rng),
         }
     }
 
     fn gen_piece_uniform<R: rand::Rng>(rng: &mut R) -> Piece {
-        rng.random_range(0..(Piece::variant_len() - 1)).into()
+        let index = rng.random_range(0..(Piece::variant_len() - 1));
+        Piece::from(index)
     }
 
-    fn gen_pieces_1h2r<R: rand::Rng>(rng: &mut R, history: &PieceHistory) -> Vec<Piece> {
-        let piece = match history.back() {
-            Some(piece) => {
-                let index = rng.random_range(0..Piece::variant_len());
-                if index + 1 != Piece::variant_len() && index != piece.variant_index() {
-                    index.into()
-                } else {
-                    Self::gen_piece_uniform(rng)
-                }
-            }
-            None => Self::gen_piece_uniform(rng),
+    // NES Tetris 1H2R:
+    // There are 7 piece types, numbered 0-6. Generate a random value from 0-7.
+    // If the value is invalid (7) or matches the previous piece, reroll from 0-6.
+    fn gen_piece_1h2r<R: rand::Rng>(rng: &mut R, history: &PieceHistory) -> Piece {
+        let Some(last_piece) = history.back() else {
+            return Self::gen_piece_uniform(rng);
         };
 
-        vec![piece]
+        let index = rng.random_range(0..Piece::variant_len());
+        if index + 1 != Piece::variant_len() && index != last_piece.variant_index() {
+            index.into()
+        } else {
+            Self::gen_piece_uniform(rng)
+        }
     }
 
-    fn gen_pieces_7bag<R: rand::Rng>(rng: &mut R, _history: &PieceHistory) -> Vec<Piece> {
+    fn gen_bag<R: rand::Rng>(rng: &mut R) -> Vec<Piece> {
         let mut pieces = Piece::iter()
             .filter(|piece| !piece.is_placeholder())
-            .cloned()
-            .collect::<Vec<Piece>>();
+            .copied()
+            .collect::<Vec<_>>();
         pieces.shuffle(rng);
         pieces
     }
