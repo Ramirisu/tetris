3f24026f6b93ac3f6c2544671393a4ec30bbcd15
Ramirisu <labyrinth.ramirisu@gmail.com>
2026-09-23 01:17:51 +0800

    Refactor audio asset management to use enum iteration for sound loading

--- src/audio.rs
+++ src/audio.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
 pub fn plugin(app: &mut App) {
     app.add_message::<PlaySoundMessage>()
@@ -8,32 +10,18 @@ pub fn plugin(app: &mut App) {
 
 #[derive(Resource)]
 struct AudioAssets {
-    move_cursor: Handle<AudioSource>,
-    start_game: Handle<AudioSource>,
-    move_curr_piece: Handle<AudioSource>,
-    rotate_curr_piece: Handle<AudioSource>,
-    lock_curr_piece: Handle<AudioSource>,
-    line_clear: Handle<AudioSource>,
-    tetris_clear: Handle<AudioSource>,
-    level_up: Handle<AudioSource>,
-    game_over: Handle<AudioSource>,
+    sounds: Vec<Handle<AudioSource>>,
 }
 
 fn load_audio_assets(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(AudioAssets {
-        move_cursor: asset_server.load("sounds/sfx02.ogg"),
-        start_game: asset_server.load("sounds/sfx03.ogg"),
-        move_curr_piece: asset_server.load("sounds/sfx04.ogg"),
-        rotate_curr_piece: asset_server.load("sounds/sfx06.ogg"),
-        lock_curr_piece: asset_server.load("sounds/sfx08.ogg"),
-        line_clear: asset_server.load("sounds/sfx11.ogg"),
-        tetris_clear: asset_server.load("sounds/sfx19.ogg"),
-        level_up: asset_server.load("sounds/sfx07.ogg"),
-        game_over: asset_server.load("sounds/sfx14.ogg"),
+        sounds: PlaySoundMessage::iter()
+            .map(|message| asset_server.load(message.asset_path()))
+            .collect(),
     });
 }
 
-#[derive(Message)]
+#[derive(Message, Clone, Copy, EnumIter)]
 pub enum PlaySoundMessage {
     MoveCursor,
     StartGame,
@@ -46,24 +34,33 @@ pub enum PlaySoundMessage {
     GameOver,
 }
 
+impl PlaySoundMessage {
+    fn asset_index(&self) -> usize {
+        *self as usize
+    }
+
+    fn asset_path(&self) -> &'static str {
+        match self {
+            Self::MoveCursor => "sounds/sfx02.ogg",
+            Self::StartGame => "sounds/sfx03.ogg",
+            Self::MoveCurrPiece => "sounds/sfx04.ogg",
+            Self::RotateCurrPiece => "sounds/sfx06.ogg",
+            Self::LockCurrPiece => "sounds/sfx08.ogg",
+            Self::LineClear => "sounds/sfx11.ogg",
+            Self::TetrisClear => "sounds/sfx19.ogg",
+            Self::LevelUp => "sounds/sfx07.ogg",
+            Self::GameOver => "sounds/sfx14.ogg",
+        }
+    }
+}
+
 fn play_sound_system(
     mut commands: Commands,
     audio_assets: Res<AudioAssets>,
     mut play_sound: MessageReader<PlaySoundMessage>,
 ) {
     for event in play_sound.read() {
-        let audio = match event {
-            PlaySoundMessage::MoveCursor => &audio_assets.move_cursor,
-            PlaySoundMessage::StartGame => &audio_assets.start_game,
-            PlaySoundMessage::MoveCurrPiece => &audio_assets.move_curr_piece,
-            PlaySoundMessage::RotateCurrPiece => &audio_assets.rotate_curr_piece,
-            PlaySoundMessage::LockCurrPiece => &audio_assets.lock_curr_piece,
-            PlaySoundMessage::LineClear => &audio_assets.line_clear,
-            PlaySoundMessage::TetrisClear => &audio_assets.tetris_clear,
-            PlaySoundMessage::LevelUp => &audio_assets.level_up,
-            PlaySoundMessage::GameOver => &audio_assets.game_over,
-        }
-        .clone();
+        let audio = audio_assets.sounds[event.asset_index()].clone();
         commands.spawn((AudioPlayer(audio), PlaybackSettings::DESPAWN));
     }
 }
