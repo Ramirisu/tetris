1328881e4bf9ab1d8870bcb1cea89970428b4a61
Ramirisu <labyrinth.ramirisu@gmail.com>
2026-09-22 16:52:55 +0800

    Make the fps closer to NES NTSC/PAL.

--- src/game/tv_system.rs
+++ src/game/tv_system.rs
@@ -15,22 +15,22 @@ pub enum TVSystem {
 enum_advance::enum_advance_derive!(TVSystem);
 
 impl TVSystem {
-    pub const fn ticks_to_duration(&self, ticks: u64) -> Duration {
-        self.subticks_to_duration(ticks * 1000)
+    pub const fn ticks_to_duration(self, ticks: u64) -> Duration {
+        self.subticks_to_duration(ticks * 1_000)
     }
 
-    pub const fn subticks_to_duration(&self, ticks: u64) -> Duration {
-        Duration::from_micros(ticks * 1_000_000_000 / self.ticks_per_microsecond())
+    pub const fn subticks_to_duration(self, subticks: u64) -> Duration {
+        Duration::from_nanos(self.duration_per_tick().as_nanos() as u64 * subticks / 1_000)
     }
 
-    pub fn duration_to_ticks(&self, duration: Duration) -> u64 {
-        (duration.as_secs_f64() * self.ticks_per_microsecond() as f64 / 1_000_000.0).round() as u64
+    pub fn duration_to_ticks(self, duration: Duration) -> u64 {
+        (duration.as_secs_f64() / self.duration_per_tick().as_secs_f64()).round() as u64
     }
 
-    const fn ticks_per_microsecond(&self) -> u64 {
+    const fn duration_per_tick(self) -> Duration {
         match self {
-            TVSystem::NTSC => 60_000_000,
-            TVSystem::PAL => 50_000_000,
+            TVSystem::NTSC => Duration::from_nanos(1_000_000_000 * 10000 / 600988), // 60.0988 fps
+            TVSystem::PAL => Duration::from_nanos(1_000_000_000 * 10000 / 500070),  // 50.0070 fps
         }
     }
 }
