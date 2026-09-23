65e78eb2ffc2f7faa33960ee4303a5a85f1df34a
Ramirisu <labyrinth.ramirisu@gmail.com>
2026-09-23 20:42:52 +0800

    Refactor main function and window setup for improved organization and clarity

--- src/main.rs
+++ src/main.rs
@@ -32,53 +32,62 @@ extern crate rust_i18n;
 i18n!("locales", fallback = "en");
 
 fn main() {
-    App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        resolution: WindowResolution::new(
-                            WINDOW_WIDTH as u32,
-                            WINDOW_HEIGHT as u32,
-                        )
-                        .with_scale_factor_override(1.0),
-                        present_mode: PresentMode::AutoNoVsync,
-                        position: WindowPosition::Centered(MonitorSelection::Primary),
-                        resizable: false,
-                        enabled_buttons: EnabledButtons {
-                            minimize: true,
-                            maximize: false,
-                            close: true,
-                        },
-                        title: "TETRIS".into(),
-                        ..default()
-                    }),
-                    ..default()
-                })
-                .set(ImagePlugin::default_nearest()),
-        )
-        .add_plugins(FpsOverlayPlugin {
-            config: FpsOverlayConfig {
-                text_color: GREEN.into(),
-                enabled: ShowFPS::default().is_enabled(),
-                ..default()
+    build_app().run();
+}
+
+fn build_app() -> App {
+    let mut app = App::new();
+    app.add_plugins(
+        DefaultPlugins
+            .set(window_plugin())
+            .set(ImagePlugin::default_nearest()),
+    )
+    .add_plugins(fps_overlay_plugin())
+    .insert_resource(ClearColor(Color::BLACK)) // application background color
+    .init_state::<AppState>()
+    .insert_resource(ControllerMapping::default())
+    .add_systems(Startup, setup_camera)
+    .add_plugins((
+        audio::plugin,
+        init::plugin,
+        screens::loading::plugin,
+        screens::splash::plugin,
+        screens::language::plugin,
+        screens::game_options::plugin,
+        screens::game_levels::plugin,
+        screens::game::plugin,
+    ));
+    app
+}
+
+fn window_plugin() -> WindowPlugin {
+    WindowPlugin {
+        primary_window: Some(Window {
+            resolution: WindowResolution::new(WINDOW_WIDTH as u32, WINDOW_HEIGHT as u32)
+                .with_scale_factor_override(1.0),
+            present_mode: PresentMode::AutoNoVsync,
+            position: WindowPosition::Centered(MonitorSelection::Primary),
+            resizable: false,
+            enabled_buttons: EnabledButtons {
+                minimize: true,
+                maximize: false,
+                close: true,
             },
-        })
-        .insert_resource(ClearColor(Color::BLACK)) // application background color
-        .init_state::<AppState>()
-        .insert_resource(ControllerMapping::default())
-        .add_systems(Startup, setup_camera)
-        .add_plugins((
-            audio::plugin,
-            init::plugin,
-            screens::loading::plugin,
-            screens::splash::plugin,
-            screens::language::plugin,
-            screens::game_options::plugin,
-            screens::game_levels::plugin,
-            screens::game::plugin,
-        ))
-        .run();
+            title: "TETRIS".into(),
+            ..default()
+        }),
+        ..default()
+    }
+}
+
+fn fps_overlay_plugin() -> FpsOverlayPlugin {
+    FpsOverlayPlugin {
+        config: FpsOverlayConfig {
+            text_color: GREEN.into(),
+            enabled: ShowFPS::default().is_enabled(),
+            ..default()
+        },
+    }
 }
 
 fn setup_camera(mut commands: Commands) {
